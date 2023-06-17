70a4dd9bb88f116546fa628c2e59fbeefc1cbc33
cafrontadoe <redesjavacamilo@gmail.com>
2023-06-17 13:23:45 -0500

    first page finished, and footer finished

--- src/app/public/slider/slider.component.ts
+++ src/app/public/slider/slider.component.ts
@@ -11,25 +11,28 @@ export class SliderComponent implements OnInit {
   ngOnInit(): void {
     this.showSlides();
   }
-
+  
   showSlides() {
-    let i;
-    let slides: any = document.getElementsByClassName('mySlides');
-    let dots = document.getElementsByClassName('dot');
-    for (i = 0; i < slides?.length; i++) {
-      slides[i].style.display = 'none';
+    const slides: HTMLCollectionOf<Element> = document.getElementsByClassName('mySlides');
+    const dots: HTMLCollectionOf<Element> = document.getElementsByClassName('dot');
+  
+    for (let i = 0; i < slides.length; i++) {
+      (slides[i] as HTMLElement).style.display = 'none';
     }
+  
     this.slideIndex++;
     if (this.slideIndex > slides.length) {
       this.slideIndex = 1;
     }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(' active', '');
+  
+    for (let i = 0; i < dots.length; i++) {
+      dots[i].classList.remove('active');
     }
+  
     if (slides[this.slideIndex - 1] && dots[this.slideIndex - 1]) {
-      slides[this.slideIndex - 1].style.display = 'block';
-      dots[this.slideIndex - 1].className += ' active';
-      setTimeout(() => this.showSlides(), 2500); // Change image every 2 seconds
+      (slides[this.slideIndex - 1] as HTMLElement).style.display = 'block';
+      dots[this.slideIndex - 1].classList.add('active');
+      setTimeout(() => this.showSlides(), 2500); // Change slide every 2 seconds
     }
   }
-}
+  }
