a80f93f67320b9ba3e130a0193ec2c9a7730275b
Camilo Andrés Frontado Escobar <redesjavacamilo@gmail.com>
2023-07-12 12:38:07 +0100

    conf envrn parcially

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
 export const environment = {
     production: true,
-    RECAPTCHA: {
-        SITE_KEY: '6Lf7UL0cAAAAAIt_m-d24WG4mA1XFPHE8yVckc5S',
-    },
+    RECAPTCHA_SITE_KEY: '6Lf7UL0cAAAAAIt_m-d24WG4mA1XFPHE8yVckc5S',
     PLACE_ID: 'ChIJX5qshlphYAwRaOTvfq48S1A'
 };
